2a98088a202d7aa8306a76c09e02514bf7ba6941
ribm205 <ribm205@gmail.com>
2025-08-14 17:50:33 -0600

    odometry fixed, bug fix urdf to use foxglove

--- build/sparkfun_qwiic_pir/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/chabots/ros2_ws/install/sparkfun_qwiic_pir'
--- build/sparkfun_top_phat_button/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/chabots/ros2_ws/install/sparkfun_top_phat_button'
